a0cc6bcdcfdd341852bd0bd3029e7e9bcf19c446
Andrey Gulitskiy <wyorma@gmail.com>
2016-07-13 12:29:54 +0600

    Первый коммит

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>My Little Blog</title>
+    </head>
+    <body>
+        <h1><?php echo 'My Little Blog'; ?></h1>
+    </body>
+</html>
