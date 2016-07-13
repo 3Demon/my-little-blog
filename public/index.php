9379dc573a68431c8ea47039c3184a912ddef8eb
3Demon <3demon@bk.ru>
2016-07-13 17:51:57 +0600

    connect & select

--- public/index.php
+++ public/index.php
@@ -20,6 +20,20 @@
     $result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
     if ($result) {
         echo "Выполнение запроса прошло успешно";
+
+        $rows = mysqli_num_rows($result); // количество полученных строк
+
+        echo "<table><tr><th>Id</th><th>Автор</th><th>Время</th><th>Текст</th></tr>";
+        for ($i = 0; $i < $rows; ++$i) {
+            $row = mysqli_fetch_row($result);
+            echo "<tr>";
+            for ($j = 0; $j < 4; ++$j) echo "<td>$row[$j]</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        // очищаем результат
+        mysqli_free_result($result);
     }
     // закрываем подключение
     mysqli_close($link);
