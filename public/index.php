48d751a2eaaa76d2c6415ce9497a37c16a30b805
Andrey Gulitskiy <wyorma@gmail.com>
2016-07-13 13:00:21 +0600

    Структура папок для фронтенда

--- public/index.php
+++ public/index.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>My Little Blog</title>
+
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
         <h1><?php echo 'My Little Blog'; ?></h1>
+
+        <script src="js/main.js"></script>
     </body>
 </html>
