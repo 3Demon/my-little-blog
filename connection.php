b4d960ee74529b24e531b92f438b5895e6d681a7
3Demon <3demon@bk.ru>
2016-07-13 17:17:43 +0600

    ololo

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+$host = 'localhost'; // адрес сервера
+$database = 'blog_database'; // имя базы данных
+$user = 'root'; // имя пользователя
+$password = ''; // пароль
--- public/index.php
+++ public/index.php
@@ -5,6 +5,24 @@
         <title>My Little Blog</title>
     </head>
     <body>
-        <h1><?php echo 'My Little Blog'; ?></h1>
+    <h1><?php echo 'My Little Bloh'; ?></h1>
     </body>
+
+    <?php
+    require_once __DIR__ . '/../connection.php'; // подключаем скрипт
+
+    // подключаемся к серверу
+    $link = mysqli_connect($host, $user, $password, $database)
+    or die("Ошибка " . mysqli_error($link));
+
+    // выполняем операции с базой данных
+    $sql = "SELECT * FROM `posts` LIMIT 0, 30 ";
+    $result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
+    if ($result) {
+        echo "Выполнение запроса прошло успешно";
+    }
+    // закрываем подключение
+    mysqli_close($link);
+    ?>
+
 </html>
